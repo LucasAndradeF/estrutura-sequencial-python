5bdf8f958ff940e54787bdfb7c878c2f1bd08c21
LucasAndradeF <lucasandradef44@gmail.com>
2023-11-01 20:14:15 -0300

    Add Exercícios Estrutura Sequencia

---
+++ .vscode/problematerreno.py
@@ -0,0 +1,12 @@
+#Problema Terreno
+
+largura_terreno = float(input("Digite a largura do terreno: "))
+comprimento_terreno = float(input("Digite comprime do terreno: "))
+valor_m_quadrado = float(input("Digite valor do metro quadrado: "))
+
+area_terreno = largura_terreno * comprimento_terreno
+preco_terreno = valor_m_quadrado * area_terreno
+
+print(f"Área do terreno: {area_terreno: .2f} m²")
+print(f"Preço do terreno: R${preco_terreno: .2f}")
+
